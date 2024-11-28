a129a647bdd29722bc1ad0541dfbbb19b5c4b24d
Чингис <underchina@mail.ru>
2024-11-28 23:53:09 +0800

    add request and connect to pdo

---
+++ Database.php
@@ -0,0 +1,15 @@
+<?php
+namespace core;
+use pdo;
+
+class Database
+{
+    protected static PDO $pdo;
+    public static function getPDO(){
+        if(!isset(self::$pdo)){
+            self::$pdo = new PDO('pgsql:host=postgres;port=5432;dbname=mydb', 'user', 'pass');
+        }
+        return self::$pdo;
+    }
+
+}
